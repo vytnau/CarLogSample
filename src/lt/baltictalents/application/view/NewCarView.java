4abcba5de7310edb4f48765d8a3e9b4fab51c331
Vytautas Naudžius <Vytautas.Naudzius@gmail.com>
2018-12-16 12:32:37 +0200

    logic for submit button and fields validation

--- src/lt/baltictalents/application/view/NewCarView.java
+++ src/lt/baltictalents/application/view/NewCarView.java
@@ -1,10 +1,13 @@
 package lt.baltictalents.application.view;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -14,6 +17,7 @@ import javafx.scene.layout.Priority;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 public class NewCarView extends Application{	
 	
@@ -28,7 +32,24 @@ public class NewCarView extends Application{
 	private static final String FUEL_TANK_LABEL = "Fuel Tank Capacity:";
 	private static final String SUBMIT_BUTTON = "Submit";
 	private static final String CANCEL_BUTTON = "Cancel";
+	private static final String ERROR_WINDOW = "Car Registration Error!";
+	private static final String CAR_MAKE_ERROR = "Please enter car make";
+	private static final String CAR_MODEL_ERROR = "Please enter car model";
+	private static final String CAR_LIC_NUMB_ERROR = "Please enter car registration plate number";
+	private static final String CAR_YEAR_ERROR = "Please enter car year";
+	private static final String CAR_ENGINE_ERROR = "Please enter car engine";
+	private static final String CAR_FUEL_TANK_ERROR = "Please enter car fuel tank capacity";
+	private static final String CAR_FUEL_TYPE_ERROR = "Please enter car fuel type";
+	private static final String REGISTRATION_SUCCESS = "Registration Successful!";
+	private static final String EMPTY_TXT = "";
 	
+	private TextField makeField;
+	private TextField modelField;
+	private TextField licNumbField;
+	private TextField yearField;
+	private TextField engineField;
+	private TextField fuelTankField;
+	private TextField fuelTypeField;
 	
 	private GridPane createRegistrationFormPane() {
 	    GridPane gridPane = new GridPane();
@@ -55,43 +76,43 @@ public class NewCarView extends Application{
 	    
 	    Label makeLabel = new Label(MAKE_LABEL);
 	    gridPane.add(makeLabel, 0,1);
-	    TextField makeField = new TextField();
+	    makeField = new TextField();
 	    makeField.setPrefHeight(40);
 	    gridPane.add(makeField, 1,1);
 
 	    Label modelLabel = new Label(MODEL_LABEL);
 	    gridPane.add(modelLabel, 0, 2);
-	    TextField modelField = new TextField();
+	    modelField = new TextField();
 	    modelField.setPrefHeight(40);
 	    gridPane.add(modelField, 1, 2);
 	    
 	    Label licNumbLabel = new Label(LICENSE_NUMBER_LABEL);
 	    gridPane.add(licNumbLabel, 0, 3);
-	    TextField licNumbField = new TextField();
+	    licNumbField = new TextField();
 	    licNumbField.setPrefHeight(40);
 	    gridPane.add(licNumbField, 1, 3);
 	    
 	    Label yearLabel = new Label(YEAR_LABEL);
 	    gridPane.add(yearLabel, 0, 4);
-	    TextField yearField = new TextField();
+	    yearField = new TextField();
 	    yearField.setPrefHeight(40);
 	    gridPane.add(yearField, 1, 4);
 	    
 	    Label engineLabel = new Label(ENGIINE_LABEL);
 	    gridPane.add(engineLabel, 0, 5);
-	    TextField engineField = new TextField();
+	    engineField = new TextField();
 	    engineField.setPrefHeight(40);
 	    gridPane.add(engineField, 1, 5);
 	    
 	    Label fuelTankLabel = new Label(FUEL_TANK_LABEL);
 	    gridPane.add(fuelTankLabel, 0, 6);
-	    TextField fuelTankField = new TextField();
+	    fuelTankField = new TextField();
 	    fuelTankField.setPrefHeight(40);
 	    gridPane.add(fuelTankField, 1, 6);
 	    
 	    Label fuelTypeLabel = new Label(FUEL_TYPE_LABEL);
 	    gridPane.add(fuelTypeLabel, 0, 7);
-	    TextField fuelTypeField = new TextField();
+	    fuelTypeField = new TextField();
 	    fuelTypeField.setPrefHeight(40);
 	    gridPane.add(fuelTypeField, 1, 7);
 
@@ -100,6 +121,7 @@ public class NewCarView extends Application{
 	    submitButton.setDefaultButton(true);
 	    submitButton.setPrefWidth(100);
 	    gridPane.add(submitButton, 0, 8);
+	    addSubmitButtonAction(submitButton, gridPane);
 	    
 	    Button cancelButton = new Button(CANCEL_BUTTON);
 	    cancelButton.setPrefHeight(40);
@@ -107,6 +129,57 @@ public class NewCarView extends Application{
 	    cancelButton.setPrefWidth(100);	    
 	    gridPane.add(cancelButton, 1, 8);	    
 	}
+	
+	private void addSubmitButtonAction(Button button, GridPane gridPane){
+		button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if(makeField.getText().isEmpty()) {
+                    showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), ERROR_WINDOW, CAR_MAKE_ERROR);
+                    return;
+                }
+                if(modelField.getText().isEmpty()) {
+                	showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), ERROR_WINDOW, CAR_MODEL_ERROR);
+                    return;
+                }
+                if(licNumbField.getText().isEmpty()) {
+                	showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), ERROR_WINDOW, CAR_LIC_NUMB_ERROR);
+                    return;
+                }
+                
+                if(yearField.getText().isEmpty()) {
+                	showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), ERROR_WINDOW, CAR_YEAR_ERROR);
+                    return;
+                }
+                
+                if(engineField.getText().isEmpty()) {
+                	showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), ERROR_WINDOW, CAR_ENGINE_ERROR);
+                    return;
+                }
+                
+                if(fuelTankField.getText().isEmpty()) {
+                	showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), ERROR_WINDOW, CAR_FUEL_TANK_ERROR);
+                    return;
+                }
+                
+                if(fuelTypeField.getText().isEmpty()) {
+                	showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), ERROR_WINDOW, CAR_FUEL_TYPE_ERROR);
+                    return;
+                }
+
+                showAlert(Alert.AlertType.CONFIRMATION, gridPane.getScene().getWindow(), REGISTRATION_SUCCESS, EMPTY_TXT);
+            }
+        });
+	}
+	
+	private void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.initOwner(owner);
+        alert.show();
+    }
 
 	@Override
     public void start(Stage primaryStage) throws Exception {
